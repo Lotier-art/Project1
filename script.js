7c3de73b424acc8a23a5eac06cdfdfddc7a5d5c5
Dima <dima@Dimas-MacBook-Air.local>
2020-11-23 11:17:49 +0200

    add script file

---
+++ script.js
@@ -0,0 +1,19 @@
+// "use strict";
+
+// a = 15;
+// console.log(a);
+
+let lotier = 10;
+const leftBorderWidth = 15;
+
+
+lotier = 15;
+console.log(lotier)
+
+const obj = {
+    a: 50 
+};
+
+obj.a = 10;
+
+console.log(obj)
